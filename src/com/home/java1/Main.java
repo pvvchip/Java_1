2347a238c74a66778233690d77f7373ea29b45f0
pvvchip <pvvchip@mail.ru>
2018-07-20 00:19:05 +0300

    lesson_2

--- src/com/home/java1/Main.java
+++ src/com/home/java1/Main.java
@@ -1,5 +1,7 @@
 package com.home.java1;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -29,13 +31,55 @@ public class Main {
             q[i][i] = 1;
         }
 
-
 //        5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
-//        6. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true если в массиве есть место, в котором сумма левой и правой части массива равны. Примеры: checkBalance([1, 1, 1, || 2, 1]) → true, checkBalance ([2, 1, 1, 2, 1]) → false, checkBalance ([10, || 10]) → true, граница показана символами ||, эти символы в массив не входят.
-//        7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным), при этом метод должен сместить все элементымассива на n позиций. Для усложнения задачи нельзя пользоваться вспомогательными массивами.
+        int[] MinMax = {12, 45, 57, 99, 234, 2, 783};
+        int min = MinMax[0];
+        int max = MinMax[0];
+        for (int i = 1; i < MinMax.length; i++){
+            if (MinMax[i] < min) min = MinMax[i];
+            if (MinMax[i] > max) max = MinMax[i];
+        }
 
+        int[] mas = {1, 1, 1, 2, 1, 2, 2, 2};
+        System.out.println("mas=" + Arrays.toString(mas));
+        System.out.println("checkBalance: " + checkBalance(mas));
 
-
-
+        int st = 2;
+        System.out.println("shift " + st + ": " + Arrays.toString(shift(mas, st)));
     }
+
+//    6. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true
+//              если в массиве есть место, в котором сумма левой и правой части массива равны.
+//              Примеры:    checkBalance([1, 1, 1, || 2, 1]) → true,
+//                          checkBalance ([2, 1, 1, 2, 1]) → false,
+//                          checkBalance ([10, || 10]) → true,
+//              граница показана символами ||, эти символы в массив не входят.
+    private static boolean checkBalance(int[] i) {
+        int leftSum = 0;
+        int leftI = 0;
+        int rightSum = 0;
+        int rightI = i.length - 1;
+
+        while (leftI <= rightI){
+            if (leftSum <= rightSum){
+              leftSum += i[leftI];
+              leftI++;
+            } else {
+                rightSum += i[rightI];
+                rightI--;
+            }
+        }
+        return (leftSum == rightSum);
+    }
+
+//     7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным,
+//          или отрицательным), при этом метод должен сместить все элементымассива на n позиций.
+//          Для усложнения задачи нельзя пользоваться вспомогательными массивами.
+    private static int[] shift(int[] mas, int st){
+
+        // в разработке
+
+        return mas;
+    }
+
 }
